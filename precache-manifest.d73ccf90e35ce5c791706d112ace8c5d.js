d68a396df74df5a60608809b5a4428501c49ac35
bh2o <tenev.bh2o@gmail.com>
2019-11-15 15:31:44 +0200

    0.0.7

---
+++ precache-manifest.d73ccf90e35ce5c791706d112ace8c5d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63d8c1a3d86afe56db50079ae0d402a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b31ab10651692cc7d87",
+    "url": "/static/css/main.efe5f74a.css"
+  },
+  {
+    "revision": "3b31ab10651692cc7d87",
+    "url": "/static/js/main.373fe78a.js"
+  },
+  {
+    "revision": "1d31b971cf20f4e8bf86bb09c2597839",
+    "url": "/static/js/main.373fe78a.js.LICENSE"
+  }
+]);
